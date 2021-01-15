d84e29c05be15e651c9e02fd8abf2c5251c8a8cf
Sohaib Furqan <sohaibfurqan@hotmail.com>
2021-01-15 17:18:08 +0500

    Persisted data to local storage

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const clearTasksBtn = document.querySelector('.clear-tasks');
 loadEventListeners();
 
 function loadEventListeners(){
+
+  // DOM Loaded
+  document.addEventListener('DOMContentLoaded',getTasks);
   form.addEventListener('submit', addTask);
 
   // Event Delegation
@@ -20,6 +23,29 @@ function loadEventListeners(){
   clearTasksBtn.addEventListener('click',clearTasks);
 }
 
+// get tasks
+function getTasks(){
+  let tasks;
+  if(localStorage.getItem('tasks')===null){
+    tasks=[];
+  } else{
+    tasks=JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.forEach(function(task){
+    const li= document.createElement('li');
+    //Add class
+    li.className='collection-item';
+    li.appendChild(document.createTextNode(task));
+    const link = document.createElement('a');
+    link.setAttribute('href','#');
+    link.className='delete-item secondary-content';
+    link.innerHTML='<i class="fas fa-times"></i>';
+    li.appendChild(link);
+    taskList.appendChild(li);
+  });
+}
+// Add tasks
 function addTask(e){
   if(inputField.value===''){
     alert('Please enter a task name');
@@ -35,6 +61,9 @@ function addTask(e){
   link.innerHTML='<i class="fas fa-times"></i>';
   li.appendChild(link);
   taskList.appendChild(li);
+
+  // Persist task to Local Storage
+  addToLocalStorage(inputField.value);
   alert('Task added!');
   //clear input field
   inputField.value='';
@@ -46,9 +75,9 @@ function removeTasks(e){
 if(e.target.parentElement.classList.contains('delete-item')){
   if(confirm('Are you sure?')){
     e.target.parentElement.parentElement.remove();
+    removeFromLocalStorage(e.target.parentElement.parentElement);
   }
 }
-
   e.preventDefault();
 }
 
@@ -77,12 +106,43 @@ function clearTasks(e){
         while(taskList.firstChild){
           taskList.removeChild(taskList.firstChild);
         }
+        localStorage.clear()
       }
-     
-    
   }
-
   //OR taskList.innerHTML ='';
   e.preventDefault();
 }
 
+
+function addToLocalStorage(task){
+  let tasks;
+  // check Local Storage
+  if(localStorage.getItem('tasks')===null){
+    tasks=[];
+  } else{
+    tasks= JSON.parse(localStorage.getItem('tasks'));
+  }
+  tasks.push(task);
+      //update local storage
+      localStorage.setItem('tasks',JSON.stringify(tasks));
+}
+
+function removeFromLocalStorage(listItem){
+  let tasks;
+  if(localStorage.getItem('tasks')===null){
+    tasks=[];
+    alert('No Tasks Added');
+  }
+  else{
+    tasks= JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.forEach(function(task,index){
+    if(task==listItem.textContent){
+      tasks.splice(index,1);
+    }
+  });
+
+  localStorage.setItem('tasks',JSON.stringify(tasks));
+
+}
